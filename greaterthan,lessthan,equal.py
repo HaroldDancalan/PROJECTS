68d030b3e20473ef484d5c29f1c0ab7129707e13
HaroldDancalan <113648236+HaroldDancalan@users.noreply.github.com>
2022-09-17 17:17:07 +0800

    GREATER THAN, LESS THAN OR EQUAL
    -it determines the numbers if it is greater than, less than, or equal

---
+++ greaterthan,lessthan,equal.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+if (num1 < num2):
+    print(num1, "is less than to", num2)
+elif (num1 > num2):
+    print(num1, "is greater than to", num2)
+elif (num2 == num2):
+    print(num1, "is equal to", num2)
