fac4c1ce1a1bb7ddb984d38cd8cb72899c973142
HaroldDancalan <113648236+HaroldDancalan@users.noreply.github.com>
2022-11-27 22:42:35 +0800

    Add files via upload
    It is a Python program that reverse the order of the items in the array

---
+++ python_array.py
@@ -0,0 +1,6 @@
+from array import *
+array_num = array('i', [9, 18, 27, 36, 45, 54, 63, 72, 81, 90])
+print("Original sequence of ninetimes using array: "+str(array_num))
+array_num.reverse()
+print("Reverse sequence of ninetimes:")
+print(str(array_num))
